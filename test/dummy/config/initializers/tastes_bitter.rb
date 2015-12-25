9ea1582a185d244176619591a513e799bbcf9c82
Mat Harvard <mat.harvard@gmail.com>
2015-12-24 17:10:13 -0800

    Update dummy app configuration

---
+++ test/dummy/config/initializers/tastes_bitter.rb
@@ -0,0 +1,7 @@
+TastesBitter.configure do |config|
+
+  config.mailer_receiver = "notificationscustom1@example.com"
+  config.mailer_sender = "notificationscustom2@example.com"
+  config.mailer_subject_prefix = "[JavaScript custom Error] "
+
+end
