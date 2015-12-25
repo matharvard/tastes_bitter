1b08775c175402ab43fd9ef75f318ea0a58328df
Mat Harvard <mat.harvard@gmail.com>
2015-12-24 17:09:44 -0800

    Set up mailer

--- app/mailers/tastes_bitter/javascript_errors_mailer.rb
+++ app/mailers/tastes_bitter/javascript_errors_mailer.rb
@@ -2,10 +2,12 @@ module TastesBitter
   class JavascriptErrorsMailer < ApplicationMailer
 
     def javascript_error(error_info)
+      @error_info = error_info
+
       mail(
         to: ::TastesBitter.configuration.mailer_receiver,
         from: ::TastesBitter.configuration.mailer_sender,
-        subject: ::TastesBitter.configuration.mailer_subject
+        subject: "#{ ::TastesBitter.configuration.mailer_subject_prefix } #{ @error_info[:message] }"
       )
     end
 
