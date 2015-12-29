f914df8a69b70d9ad1d4f490b7760712ca554e55
Mat Harvard <mat.harvard@gmail.com>
2015-12-29 00:35:40 -0800

    Add some controller tests

--- test/controllers/tastes_bitter/javascript_errors_controller_test.rb
+++ test/controllers/tastes_bitter/javascript_errors_controller_test.rb
@@ -1,13 +1,34 @@
 require 'test_helper'
 
 module TastesBitter
+
   class JavascriptErrorsControllerTest < ActionController::TestCase
-    setup do
-      @routes = Engine.routes
-    end
+
+    # setup do
+    #   @routes = Engine.routes
+    # end
 
     # test "the truth" do
     #   assert true
     # end
+    
+    test "should respond with ok status without params" do
+      xhr :post, :create
+      assert_response :ok
+    end
+
+    test "should respond with ok status with params" do
+      params = {
+        "message": "A message",
+        "file_or_page": "A file or page",
+        "line_number": "A line number",
+        "user_agent": "A user agent",
+        "current_page": "A current page"
+      }
+      xhr :post, :create, params
+      assert_response :ok
+    end
+
   end
+
 end
