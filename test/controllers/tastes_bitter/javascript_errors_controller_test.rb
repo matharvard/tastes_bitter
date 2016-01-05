11cbb20c6dba95dbcac5d3e712e25d1d96015f7e
Mat Harvard <mat.harvard@gmail.com>
2016-01-04 23:23:49 -0800

    Add a test for some params and all params

--- test/controllers/tastes_bitter/javascript_errors_controller_test.rb
+++ test/controllers/tastes_bitter/javascript_errors_controller_test.rb
@@ -13,13 +13,27 @@ module TastesBitter
       assert_response :ok
     end
 
-    test "should respond with ok status with params" do
+    test "should respond with ok status with some params" do
       params = {
         "message": "A message",
         "file_or_page": "A file or page",
         "line_number": "A line number",
         "user_agent": "A user agent",
         "current_page": "A current page"
+      }
+      xhr :post, :create, params
+      assert_response :ok
+    end
+
+    test "should respond with ok status with all params" do
+      params = {
+        "message": "A message",
+        "file_or_page": "A file or page",
+        "line_number": "A line number",
+        "user_agent": "A user agent",
+        "current_page": "A current page",
+        "column_number": "A column number",
+        "stack_trace": "A stack trace"
       }
       xhr :post, :create, params
       assert_response :ok
